8065db64a0fa3deee138b63348e89e72b5a4dd9e
This-swapnil <sswapnil0098@gmail.com>
2024-01-05 19:09:46 +0530

    stack_using_inheriting_linked_list

---
+++ Stack/stack_using_inheriting_linked_list.py
@@ -0,0 +1,45 @@
+from List.Singly_Linked_List import *
+
+
+class Stack(singly_link_list):
+    def __init__(self) -> None:
+        super().__init__()
+        self.item_count = 0
+
+    def is_empty(self):
+        return super().is_empty()
+
+    def push(self, data):
+        self.insert_at_start(data=data)
+        self.item_count += 1
+
+    def pop(self):
+        if not self.is_empty():
+            self.delete_first()
+            self.item_count -= 1
+        else:
+            raise IndexError("Stack is empty")
+
+    def peek(self):
+        if not self.is_empty():
+            return self.start.item
+        else:
+            raise IndexError("Stack is empty")
+
+    def size(self):
+        return self.item_count
+
+
+s = Stack()
+s.push(10)
+s.push(20)
+s.push(30)
+s.push(40)
+s.push(50)
+# s.insert(1, 10)
+
+print(f"Total element in Stack: {s.size()}")
+print(f"Top element is: {s.peek()}")
+print(f"Removed element is: {s.pop()}")
+print(f"Top element is: {s.peek()}")
+print(f"Total element in Stack: {s.size()}")
